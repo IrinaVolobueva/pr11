8b6f12d88102441022466974e9ee23165c5ac7e2
IrinaVolobueva <145708238+IrinaVolobueva@users.noreply.github.com>
2024-06-30 14:16:52 +0300

    Add files via upload

--- PR1/Form1.Designer.cs
+++ PR1/Form1.Designer.cs
@@ -39,13 +39,31 @@
             this.оПрограммеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.ColdChristmasTree = new System.Windows.Forms.TextBox();
+            this.SumChristmasTree = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ColdStars = new System.Windows.Forms.TextBox();
+            this.SumStars = new System.Windows.Forms.TextBox();
+            this.ColdGarland = new System.Windows.Forms.TextBox();
+            this.SumGarland = new System.Windows.Forms.TextBox();
+            this.ColdBall = new System.Windows.Forms.TextBox();
+            this.SumBall = new System.Windows.Forms.TextBox();
+            this.ColdTinsel = new System.Windows.Forms.TextBox();
+            this.SumTinsel = new System.Windows.Forms.TextBox();
             this.Вычислить = new System.Windows.Forms.Button();
             this.Выход = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.SumTextBox = new System.Windows.Forms.TextBox();
+            this.ColdTextBox = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
             this.menuStrip1.SuspendLayout();
             this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
             this.SuspendLayout();
             // 
             // menuStrip1
@@ -56,7 +74,7 @@
             this.справкаToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(422, 28);
+            this.menuStrip1.Size = new System.Drawing.Size(791, 28);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -76,7 +94,7 @@
             this.вычислитьToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.программаНовогодниеУкрашенияToolStripMenuItem});
             this.вычислитьToolStripMenuItem.Name = "вычислитьToolStripMenuItem";
-            this.вычислитьToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.вычислитьToolStripMenuItem.Size = new System.Drawing.Size(167, 26);
             this.вычислитьToolStripMenuItem.Text = "Вычислить";
             // 
             // программаНовогодниеУкрашенияToolStripMenuItem
@@ -94,14 +112,14 @@
             // очиститьToolStripMenuItem
             // 
             this.очиститьToolStripMenuItem.Name = "очиститьToolStripMenuItem";
-            this.очиститьToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.очиститьToolStripMenuItem.Size = new System.Drawing.Size(167, 26);
             this.очиститьToolStripMenuItem.Text = "Очистить";
             this.очиститьToolStripMenuItem.Click += new System.EventHandler(this.очиститьToolStripMenuItem_Click);
             // 
             // выходToolStripMenuItem
             // 
             this.выходToolStripMenuItem.Name = "выходToolStripMenuItem";
-            this.выходToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.выходToolStripMenuItem.Size = new System.Drawing.Size(167, 26);
             this.выходToolStripMenuItem.Text = "Выход";
             this.выходToolStripMenuItem.Click += new System.EventHandler(this.выходToolStripMenuItem_Click);
             // 
@@ -123,50 +141,163 @@
             // 
             this.groupBox1.Location = new System.Drawing.Point(34, 43);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(350, 155);
+            this.groupBox1.Size = new System.Drawing.Size(719, 155);
             this.groupBox1.TabIndex = 1;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Задание";
             // 
             // groupBox2
             // 
-            this.groupBox2.Controls.Add(this.textBox2);
-            this.groupBox2.Controls.Add(this.textBox4);
-            this.groupBox2.Controls.Add(this.textBox3);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.ColdChristmasTree);
+            this.groupBox2.Controls.Add(this.SumChristmasTree);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.ColdStars);
+            this.groupBox2.Controls.Add(this.SumStars);
+            this.groupBox2.Controls.Add(this.ColdGarland);
+            this.groupBox2.Controls.Add(this.SumGarland);
+            this.groupBox2.Controls.Add(this.ColdBall);
+            this.groupBox2.Controls.Add(this.SumBall);
+            this.groupBox2.Controls.Add(this.ColdTinsel);
+            this.groupBox2.Controls.Add(this.SumTinsel);
             this.groupBox2.Location = new System.Drawing.Point(34, 213);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(350, 278);
+            this.groupBox2.Size = new System.Drawing.Size(350, 349);
             this.groupBox2.TabIndex = 2;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Входные данные";
             // 
-            // textBox2
+            // label5
             // 
-            this.textBox2.Location = new System.Drawing.Point(57, 224);
-            this.textBox2.Multiline = true;
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(258, 36);
-            this.textBox2.TabIndex = 1;
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(20, 66);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(41, 16);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Елка";
             // 
-            // textBox4
+            // ColdChristmasTree
             // 
-            this.textBox4.Location = new System.Drawing.Point(57, 66);
-            this.textBox4.Multiline = true;
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(258, 34);
-            this.textBox4.TabIndex = 2;
+            this.ColdChristmasTree.Location = new System.Drawing.Point(252, 63);
+            this.ColdChristmasTree.Multiline = true;
+            this.ColdChristmasTree.Name = "ColdChristmasTree";
+            this.ColdChristmasTree.Size = new System.Drawing.Size(55, 35);
+            this.ColdChristmasTree.TabIndex = 11;
             // 
-            // textBox3
+            // SumChristmasTree
             // 
-            this.textBox3.Location = new System.Drawing.Point(57, 152);
-            this.textBox3.Multiline = true;
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(258, 34);
-            this.textBox3.TabIndex = 1;
+            this.SumChristmasTree.Location = new System.Drawing.Point(148, 63);
+            this.SumChristmasTree.Multiline = true;
+            this.SumChristmasTree.Name = "SumChristmasTree";
+            this.SumChristmasTree.Size = new System.Drawing.Size(55, 35);
+            this.SumChristmasTree.TabIndex = 10;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(20, 279);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(79, 16);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Звездочка";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(20, 225);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 16);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Гирлянда";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(20, 173);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 16);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Шарик";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 117);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Мишура";
+            // 
+            // ColdStars
+            // 
+            this.ColdStars.Location = new System.Drawing.Point(252, 276);
+            this.ColdStars.Multiline = true;
+            this.ColdStars.Name = "ColdStars";
+            this.ColdStars.Size = new System.Drawing.Size(55, 35);
+            this.ColdStars.TabIndex = 7;
+            // 
+            // SumStars
+            // 
+            this.SumStars.Location = new System.Drawing.Point(148, 276);
+            this.SumStars.Multiline = true;
+            this.SumStars.Name = "SumStars";
+            this.SumStars.Size = new System.Drawing.Size(55, 35);
+            this.SumStars.TabIndex = 6;
+            // 
+            // ColdGarland
+            // 
+            this.ColdGarland.Location = new System.Drawing.Point(252, 222);
+            this.ColdGarland.Multiline = true;
+            this.ColdGarland.Name = "ColdGarland";
+            this.ColdGarland.Size = new System.Drawing.Size(55, 35);
+            this.ColdGarland.TabIndex = 5;
+            // 
+            // SumGarland
+            // 
+            this.SumGarland.Location = new System.Drawing.Point(148, 222);
+            this.SumGarland.Multiline = true;
+            this.SumGarland.Name = "SumGarland";
+            this.SumGarland.Size = new System.Drawing.Size(55, 35);
+            this.SumGarland.TabIndex = 4;
+            // 
+            // ColdBall
+            // 
+            this.ColdBall.Location = new System.Drawing.Point(252, 170);
+            this.ColdBall.Multiline = true;
+            this.ColdBall.Name = "ColdBall";
+            this.ColdBall.Size = new System.Drawing.Size(55, 35);
+            this.ColdBall.TabIndex = 3;
+            // 
+            // SumBall
+            // 
+            this.SumBall.Location = new System.Drawing.Point(148, 170);
+            this.SumBall.Multiline = true;
+            this.SumBall.Name = "SumBall";
+            this.SumBall.Size = new System.Drawing.Size(55, 35);
+            this.SumBall.TabIndex = 2;
+            // 
+            // ColdTinsel
+            // 
+            this.ColdTinsel.Location = new System.Drawing.Point(252, 114);
+            this.ColdTinsel.Multiline = true;
+            this.ColdTinsel.Name = "ColdTinsel";
+            this.ColdTinsel.Size = new System.Drawing.Size(55, 35);
+            this.ColdTinsel.TabIndex = 1;
+            // 
+            // SumTinsel
+            // 
+            this.SumTinsel.Location = new System.Drawing.Point(148, 114);
+            this.SumTinsel.Multiline = true;
+            this.SumTinsel.Name = "SumTinsel";
+            this.SumTinsel.Size = new System.Drawing.Size(55, 35);
+            this.SumTinsel.TabIndex = 0;
             // 
             // Вычислить
             // 
-            this.Вычислить.Location = new System.Drawing.Point(34, 519);
+            this.Вычислить.Location = new System.Drawing.Point(412, 473);
             this.Вычислить.Name = "Вычислить";
             this.Вычислить.Size = new System.Drawing.Size(140, 51);
             this.Вычислить.TabIndex = 4;
@@ -176,7 +307,7 @@
             // 
             // Выход
             // 
-            this.Выход.Location = new System.Drawing.Point(244, 519);
+            this.Выход.Location = new System.Drawing.Point(613, 473);
             this.Выход.Name = "Выход";
             this.Выход.Size = new System.Drawing.Size(140, 51);
             this.Выход.TabIndex = 5;
@@ -184,11 +315,59 @@
             this.Выход.UseVisualStyleBackColor = true;
             this.Выход.Click += new System.EventHandler(this.Выход_Click);
             // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.label7);
+            this.groupBox3.Controls.Add(this.label6);
+            this.groupBox3.Controls.Add(this.ColdTextBox);
+            this.groupBox3.Controls.Add(this.SumTextBox);
+            this.groupBox3.Location = new System.Drawing.Point(412, 213);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(341, 218);
+            this.groupBox3.TabIndex = 3;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Результат";
+            // 
+            // SumTextBox
+            // 
+            this.SumTextBox.Location = new System.Drawing.Point(168, 82);
+            this.SumTextBox.Multiline = true;
+            this.SumTextBox.Name = "SumTextBox";
+            this.SumTextBox.Size = new System.Drawing.Size(148, 35);
+            this.SumTextBox.TabIndex = 13;
+            // 
+            // ColdTextBox
+            // 
+            this.ColdTextBox.Location = new System.Drawing.Point(168, 154);
+            this.ColdTextBox.Multiline = true;
+            this.ColdTextBox.Name = "ColdTextBox";
+            this.ColdTextBox.Size = new System.Drawing.Size(148, 35);
+            this.ColdTextBox.TabIndex = 14;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(6, 85);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(96, 16);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Общая сумма:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(6, 157);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(133, 16);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Общее кол-во штук:";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(422, 596);
+            this.ClientSize = new System.Drawing.Size(791, 580);
+            this.Controls.Add(this.groupBox3);
             this.Controls.Add(this.Выход);
             this.Controls.Add(this.Вычислить);
             this.Controls.Add(this.groupBox2);
@@ -201,6 +380,8 @@
             this.menuStrip1.PerformLayout();
             this.groupBox2.ResumeLayout(false);
             this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -221,9 +402,26 @@
         private System.Windows.Forms.GroupBox groupBox2;
         private System.Windows.Forms.Button Вычислить;
         private System.Windows.Forms.Button Выход;
-        private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox ColdTinsel;
+        private System.Windows.Forms.TextBox SumTinsel;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TextBox SumStars;
+        private System.Windows.Forms.TextBox ColdGarland;
+        private System.Windows.Forms.TextBox SumGarland;
+        private System.Windows.Forms.TextBox ColdBall;
+        private System.Windows.Forms.TextBox SumBall;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox ColdChristmasTree;
+        private System.Windows.Forms.TextBox SumChristmasTree;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox ColdStars;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox ColdTextBox;
+        private System.Windows.Forms.TextBox SumTextBox;
     }
 }
 
--- PR1/Form1.cs
+++ PR1/Form1.cs
@@ -17,27 +17,36 @@ namespace PR1
             InitializeComponent();
         }
         private void Вычислить_Click(object sender, EventArgs e)
-        {
-            try
+        { 
+            if (!String.IsNullOrEmpty(SumChristmasTree.Text) & !String.IsNullOrEmpty(ColdChristmasTree.Text) & !String.IsNullOrEmpty(SumTinsel.Text) & !String.IsNullOrEmpty(ColdTinsel.Text) & 
+                !String.IsNullOrEmpty(SumBall.Text) & !String.IsNullOrEmpty(ColdBall.Text) & !String.IsNullOrEmpty(SumGarland.Text) & !String.IsNullOrEmpty(ColdGarland.Text) & !String.IsNullOrEmpty(SumStars.Text) & 
+                !String.IsNullOrEmpty(ColdStars.Text))
             {
-                if (!String.IsNullOrEmpty(textBox2.Text) & !String.IsNullOrEmpty(textBox3.Text) & !String.IsNullOrEmpty(textBox4.Text))
+                if (Double.TryParse(SumChristmasTree.Text, out double sumTree) & Int32.TryParse(ColdChristmasTree.Text, out int coldTree) & Double.TryParse(SumTinsel.Text, out double sumTinsel) & 
+                    Int32.TryParse(ColdTinsel.Text, out int coldTinsel) & Double.TryParse(SumBall.Text, out double sumBall) & Int32.TryParse(ColdBall.Text, out int coldBall) & Double.TryParse(SumGarland.Text, out double sumGarland) &
+                    Int32.TryParse(ColdGarland.Text, out int coldGarland) & Double.TryParse(SumStars.Text, out double sumStars) & Int32.TryParse(ColdStars.Text, out int coldStars))
                 {
-                    ChristmasDecorations decoration = new ChristmasDecorations
+                    if (sumTree >= 0 & coldTree >= 0 & sumTinsel >= 0 & coldTinsel >= 0 & sumBall >= 0 & coldTinsel >= 0 & sumBall >= 0 & coldBall >= 0 & sumGarland >= 0 & coldGarland >= 0 & sumStars >= 0 & coldStars >= 0)
                     {
-                        type = textBox4.Text,
-                        color = textBox3.Text,
-                        quantity = UInt32.Parse(textBox2.Text)
-                    };
-                     MessageBox.Show($"Количество украшений на елке: {decoration.CalculateOnTree()}", "Результат", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        double sum = Math.Round((sumTree * coldTree + sumTinsel * coldTinsel + sumBall * coldBall + sumGarland * coldBall + sumStars * coldStars), 3);
+                        SumTextBox.Text = $"{sum}";
+
+                        int cold = coldTree + coldTinsel + coldBall + coldGarland + coldStars;
+                        ColdTextBox.Text = $"{cold}";
+                    }
+                    else
+                    {
+                        MessageBox.Show("Сумма и кол-во украшений для нового года должны быть больше 0!", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
                 }
-                else 
-                { 
-                    MessageBox.Show("Проверьте не пустые ли у вас строчки!", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error); 
+                else
+                {
+                    MessageBox.Show("Проверьте введенность данных.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 }
             }
-            catch (FormatException)
+            else
             {
-                MessageBox.Show("Проверьте введенные данные!", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                MessageBox.Show("Проверьте введенность данных.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
         private void Выход_Click(object sender, EventArgs e)
@@ -47,34 +56,14 @@ namespace PR1
 
         private void очиститьToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            textBox2.Clear();
-            textBox3.Clear();
-            textBox4.Clear();
+            ColdTinsel.Clear();
+            SumBall.Clear();
+            ColdBall.Clear();
         }
 
         private void программаНовогодниеУкрашенияToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            try
-            {
-                if (!String.IsNullOrEmpty(textBox2.Text) & !String.IsNullOrEmpty(textBox3.Text) & !String.IsNullOrEmpty(textBox4.Text))
-                {
-                    ChristmasDecorations decoration = new ChristmasDecorations
-                    {
-                        type = textBox4.Text,
-                        color = textBox3.Text,
-                        quantity = UInt32.Parse(textBox2.Text)
-                    };
-                    MessageBox.Show($"Количество украшений на елке: {decoration.CalculateOnTree()}", "Результат", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                }
-                else
-                {
-                    MessageBox.Show("Проверьте не пустые ли у вас строчки!", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                }
-            }
-            catch (FormatException)
-            {
-                MessageBox.Show("Проверьте введенные данные!", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
+            
         }
 
         private void выходToolStripMenuItem_Click(object sender, EventArgs e)
